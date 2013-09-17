5f8db0b06450548b7f3f7e78a57bf120b2fe84ae
William Ngo <will@mrngoitall.net>
2013-09-17 16:24:02 -0700

    Separated directives for checkbox

--- public/js/directives.js
+++ public/js/directives.js
@@ -16,17 +16,17 @@ angular.module('mean.polls', [])
     restrict: 'A',
     template: '<td>{{ votes[invitee.user].name }}</td>' +
               '<td ng-repeat="choice in poll.choices | orderBy:\'order\'">' +
-              '<input type="checkbox" ng-model="votes[invitee.user][choice._id]">' +
+              '<vote-checkbox></vote-checkbox>' +
               '</td>',
     link: function(scope, ele, attrs, ctrl) {
-      //scope.$apply(scope.findVotes());
-
+    }
+  };
+})
+.directive('voteCheckbox', function() {
+  return {
+    restrict: 'E',
+    template: '<input type="checkbox" ng-model="votes[invitee.user][choice._id]">',
+    link: function(scope, ele, attrs, ctrl) {
     }
   };
 });
-
-/*
-  
-    
-  
-*/
