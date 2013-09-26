17be2e882aa3afe42981bf8348c54c3d882e237b
William Ngo <will@mrngoitall.net>
2013-09-25 21:17:09 -0700

    Added filters to support displaying new Google Places fields

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', '$strap.directives','mean.system', 'mean.polls']);
+window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', '$strap.directives','mean.system', 'mean.polls', 'mean.filters']);
 
 angular.module('mean.system', []);
 angular.module('mean.polls', []);
---
+++ public/js/filters.js
@@ -0,0 +1,19 @@
+angular.module('mean.filters', [])
+.filter('removeZeros', function() {
+  return function(rating) {
+    if (rating > 0) {
+      return rating;
+    } else {
+      return '';
+    }
+  }
+})
+.filter('priceRateFormat', function() {
+  return function(priceRating) {
+    var rating = '';
+    for (var i = 0; i < priceRating; i++) {
+      rating += '$';
+    }
+    return rating;
+  }
+})
