a1f82b0da45c007daf4c791d1627254a62fd2485
William Ngo <will@mrngoitall.net>
2013-09-29 21:40:44 -0700

    Removed exports from server.js file, per Larry's suggestions

--- server.js
+++ server.js
@@ -11,40 +11,37 @@ var express = require('express'),
  * Please note that the order of loading is important.
  */
 
-//Load configurations
-//if test env, load example file
+// Load configurations
+// if test env, load example file
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     config = require('./config/config'),
     auth = require('./config/middlewares/authorization'),
     mongoose = require('mongoose');
 
-//Bootstrap db connection
+// Bootstrap db connection
 var db = mongoose.connect(config.db);
 
-//Bootstrap models
+// Bootstrap models
 var models_path = __dirname + '/app/models';
 fs.readdirSync(models_path).forEach(function(file) {
     require(models_path + '/' + file);
 });
 
-//bootstrap passport config
+// Bootstrap passport config
 require('./config/passport')(passport);
 
 var app = express();
 
-//express settings
+// Express settings
 require('./config/express')(app, passport);
 
-//Bootstrap routes
+// Bootstrap routes
 require('./config/routes')(app, passport, auth);
 
-//Start the app by listening on <port>
+// Start the app by listening on <port>
 var port = config.port;
 app.listen(port);
 console.log('Express app started on port ' + port);
 
-//Initializing logger 
+// Initializing logger 
 logger.init(app, passport, mongoose);
-
-//expose app
-exports = module.exports = app;
